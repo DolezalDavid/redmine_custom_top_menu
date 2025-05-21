e86b48535155b027697a39bdb91b156f0eec2426
DolezalDavid <52913270+DolezalDavid@users.noreply.github.com>
2025-05-21 10:38:11 +0200

    Update welcome_controller_patch.rb

--- lib/redmine_custom_top_menu/welcome_controller_patch.rb
+++ lib/redmine_custom_top_menu/welcome_controller_patch.rb
@@ -1,25 +1,23 @@
 module RedmineCustomTopMenu
-  class Hooks < Redmine::Hook::ViewListener
-    # Set up the custom menu
-    def self.setup_menu
-      # Delete the help menu item
-      Redmine::MenuManager.map(:top_menu).delete(:help)
-      
-      # Add new menu items
-      Redmine::MenuManager.map(:top_menu) do |menu|
-        menu.push :issues, { :controller => 'issues', :action => 'index' }, 
-                 :caption => :label_issue_plural, :after => :projects
-                 
-        menu.push :time_entries, { :controller => 'timelog', :action => 'index' },
-                 :if => Proc.new {
-                   User.current.allowed_to?(:view_time_entries, nil, :global => true) &&
-                   EnabledModule.exists?(:project => Project.visible, :name => :time_tracking)
-                 },
-                 :caption => :label_spent_time, :after => :issues
+  module WelcomeControllerPatch
+    def self.included(base)
+      base.class_eval do
+        # Přepíše metodu index
+        def index
+          if User.current.logged?
+            redirect_to my_page_path
+          else
+            # Pro nepřihlášené uživatele zachovat původní chování
+            @news = News.latest User.current
+            @projects = Project.latest User.current
+          end
+        end
       end
     end
   end
 end
 
-# Set up the menu on plugin initialization
-RedmineCustomTopMenu::Hooks.setup_menu
+# Přidáme patch do controlleru
+Rails.application.config.to_prepare do
+  WelcomeController.send(:include, RedmineCustomTopMenu::WelcomeControllerPatch)
+end
